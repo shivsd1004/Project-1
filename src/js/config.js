1a075e40432c8408a3938aa62628cb30696ce591
shivsd1004 <shivsd1234@gmail.com>
2024-03-08 20:58:23 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// export const API_URL ='https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const API_URL =
+  'https://api.allorigins.win/raw?url=https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL2='https://forkify-api.herokuapp.com/api/v2/recipes'
+export const TIMEOUT_SEC=10;
+export const REC_PER_PAGE=10;
+export const KEY='ca2d0f1c-42e7-41d4-8885-117846a75ba2';
+export const MODAL_CLOSE_SEC=2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,17 @@
+import View from "./View.js";
+import previewView from "./previewView.js";
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends View{
+    _parentElement=document.querySelector('.results');
+    _errorMessage='NO recipes found for querry. Please try again!';
+    _message='';
+
+    _generateMarkup(){
+        // console.log(this._data);
+        return this._data.map(result => previewView.render(result,false)).join('')
+    }
+    
+    }
+
+export default new ResultsView();
